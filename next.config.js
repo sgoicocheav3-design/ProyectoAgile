004f6d020b1a92c3eeb7865a2e85eda90ca088bb
sgoicocheav3-desing <sgoicocheav3@upao.edu.pe>
2026-05-25 03:01:00 -0500

    Fixed pasarela de pagos

--- next.config.js
+++ next.config.js
@@ -11,6 +11,26 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.mercadopago.com https://*.mercadopago.com.pe",
+              "connect-src 'self' https://*.mercadopago.com https://*.mercadopago.com.pe",
+              "frame-src 'self' https://*.mercadopago.com https://*.mercadopago.com.pe",
+              "img-src 'self' data: https://*.mercadopago.com https://*.mercadopago.com.pe",
+              "style-src 'self' 'unsafe-inline' https://*.mercadopago.com https://*.mercadopago.com.pe",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
